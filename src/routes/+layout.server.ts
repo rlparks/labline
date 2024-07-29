3f69d3f2c574869fcf9b5906035bd94f36a4bd20
Rebecca Parks <rparkstx@gmail.com>
2024-07-29 12:40:05 -0400

    Prevent unnecessary provider check when logged in

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
 import type { LayoutServerLoad } from "./$types";
 
 export const load = (async ({ fetch, locals }) => {
-	const ssoProviders = await (await fetch("/api/auth/methods")).json();
-	return { user: locals.user, ssoProviders: ssoProviders.authProviders };
+	const ssoProviders = !locals.user ? await (await fetch("/api/auth/methods")).json() : undefined;
+	return { user: locals.user, ssoProviders: ssoProviders?.authProviders };
 }) satisfies LayoutServerLoad;
