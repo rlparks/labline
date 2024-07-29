08c767fc9c9c2212d9a0ec51a1ba4112a233e4dd
Rebecca Parks <rparkstx@gmail.com>
2024-07-29 16:00:14 -0400

    Externalize building API route

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,29 +3,14 @@ import type { Building } from "$lib/types";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ locals }) => {
+export const load = (async ({ fetch, locals }) => {
 	if (!locals.user) {
 		return error(401, "Unauthorized");
 	}
-
-	try {
-		const labs = await getLabData();
-		const buildings: Building[] = labs.map((lab) => ({
-			name: lab["Bldg Name"],
-			number: lab["Bldg Number"],
-		}));
-
-		const seenBuildingNumbers = new Set<number>();
-		const uniqueBuildings = buildings.filter((building) => {
-			if (seenBuildingNumbers.has(building.number)) {
-				return false;
-			}
-			seenBuildingNumbers.add(building.number);
-			return true;
-		});
-		return { buildings: uniqueBuildings };
-	} catch (err) {
-		console.log(err);
+	const buildingRes = await fetch("/api/buildings");
+	if (!buildingRes.ok) {
+		return { buildings: [] };
 	}
-	return { buildings: [] };
+	const buildings = (await buildingRes.json()) as Building[];
+	return { buildings };
 }) satisfies PageServerLoad;
--- src/routes/api/buildings/+server.ts
+++ src/routes/api/buildings/+server.ts
@@ -1,5 +1,23 @@
+import { getLabData } from "$lib/server";
+import type { Building } from "$lib/types";
+import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async () => {
-	return new Response();
+	const labs = await getLabData();
+	const buildings: Building[] = labs.map((lab) => ({
+		name: lab["Bldg Name"],
+		number: lab["Bldg Number"],
+	}));
+
+	const seenBuildingNumbers = new Set<number>();
+	const uniqueBuildings = buildings.filter((building) => {
+		if (seenBuildingNumbers.has(building.number)) {
+			return false;
+		}
+		seenBuildingNumbers.add(building.number);
+		return true;
+	});
+
+	return json(uniqueBuildings);
 };
