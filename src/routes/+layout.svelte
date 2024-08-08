2009df4ca9531f791654d94b9c1ff547124f6625
Rebecca Parks <rparkstx@gmail.com>
2024-08-08 08:34:48 -0400

    Add error message when no JS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,14 @@
 	import "./theme.css";
 	import "./fonts.css";
 	import ESD from "$lib/images/ESD.png";
+	import { browser } from "$app/environment";
 
 	const { children, data } = $props();
+
+	if (browser) {
+		const main = document.getElementsByTagName("main").item(0);
+		if (main) main.style.display = "block";
+	}
 </script>
 
 <Header user={data.user} provider={data.ssoProviders?.[0]} />
@@ -20,10 +26,24 @@
 	{@render children()}
 </main>
 
+<noscript>
+	<div class="medium middle-align center-align">
+		<div>
+			<i class="extra">javascript</i>
+			<h5 class="center-align">Sorry, JavaScript is required to use this resource</h5>
+			<p class="center-align">Please enable JavaScript</p>
+		</div>
+	</div>
+</noscript>
+
 <style>
 	@media (max-width: 600px) {
 		img {
 			width: 300px;
 		}
+	}
+
+	main {
+		display: none;
 	}
 </style>
