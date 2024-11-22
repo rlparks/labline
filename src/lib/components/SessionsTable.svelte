7e56ddcbf5155f989c877e7dd005dcdfb597ac34
Rebecca Parks <rparkstx@gmail.com>
2024-11-22 14:56:43 -0500

    Begin work on sessions

---
+++ src/lib/components/SessionsTable.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	const title = "Sessions · username";
+</script>
+
+<svelte:head>
+	<title>knowledger · {title}</title>
+	<meta name="description" content="knowledger session management." />
+</svelte:head>
+
+<h3 class="center-align">{title}</h3>
--- src/lib/components/UsersTable.svelte
+++ src/lib/components/UsersTable.svelte
@@ -13,16 +13,25 @@
 			<th>Username</th>
 			<th>Name</th>
 			<th>Role</th>
+			<th>Sessions</th>
 			<th></th>
 		</tr>
 	</thead>
 	<tbody>
 		{#each users as user}
+			{@const numSessions = 0}
 			<tr>
 				<td>{user.username}</td>
 				<td>{user.name}</td>
 				<td>{user?.role}</td>
-				<td> <a class="button circle border" href="/admin/users/{user.id}"><i>edit</i></a></td>
+				<td>
+					<a
+						class="button circle"
+						class:border={numSessions === 0}
+						href="/admin/users/{user.id}/sessions">{numSessions}</a
+					>
+				</td>
+				<td><a class="button circle border" href="/admin/users/{user.id}"><i>edit</i></a></td>
 			</tr>
 		{/each}
 	</tbody>
