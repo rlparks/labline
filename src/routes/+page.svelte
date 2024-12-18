210bd35eedae11bde6a1f570da29158cff509ae3
Rebecca Parks <rparkstx@gmail.com>
2024-12-18 11:30:43 -0500

    Update splash text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,20 @@
 	<meta name="description" content="Access UGA Lab Emergency Contacts." />
 </svelte:head>
 
-<div class="center-align"><Title scale={1.5} /></div>
+<div class="center-align">
+	<Title scale={1.5} />
+</div>
 
 <h3 class="center-align">UGA Lab Emergency Contacts</h3>
 
+<p class="center-align">When an emergency occurs in a chemical lab, every second matters.</p>
+
 <p class="center-align">
-	Labline provides first responders with instant access to critical lab contact information when
-	every second matters.
+	Labline provides first responders with instant access to critical lab contact information.
+</p>
+
+<p class="center-align">
+	Contact the UGA Environmental Safety Division at
+	<a class="link" href="mailto:esd@uga.edu">esd@uga.edu</a>
+	to gain access.
 </p>
