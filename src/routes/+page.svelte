a1902e6505db6c902449e8a99ea2691b969420f7
Rebecca Parks <rparkstx@gmail.com>
2024-12-18 15:07:57 -0500

    Clean splash page and add More Info button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	import { Title } from "$lib/components";
 
 	const { data } = $props();
+
+	let moreInfoShown = $state(false);
 </script>
 
 <svelte:head>
@@ -9,20 +11,25 @@
 	<meta name="description" content="Access UGA Lab Emergency Contacts." />
 </svelte:head>
 
+<!-- we stack -->
 <div class="center-align">
 	<Title scale={1.5} />
+
+	<h3>UGA Lab Emergency Contacts</h3>
+
+	<p>When an emergency occurs in a chemical lab, every second matters.</p>
+
+	<p>Labline provides first responders with instant access to critical lab contact information.</p>
+
+	<p>
+		Contact the UGA Environmental Safety Division at
+		<a class="link" href="mailto:esd@uga.edu">esd@uga.edu</a>
+		to gain access.
+	</p>
+
+	<p>Select the <i>login</i> Login button in the top right to begin.</p>
+
+	<button class:border={!moreInfoShown} onclick={() => (moreInfoShown = !moreInfoShown)}>
+		<i>info</i>More Info
+	</button>
 </div>
-
-<h3 class="center-align">UGA Lab Emergency Contacts</h3>
-
-<p class="center-align">When an emergency occurs in a chemical lab, every second matters.</p>
-
-<p class="center-align">
-	Labline provides first responders with instant access to critical lab contact information.
-</p>
-
-<p class="center-align">
-	Contact the UGA Environmental Safety Division at
-	<a class="link" href="mailto:esd@uga.edu">esd@uga.edu</a>
-	to gain access.
-</p>
