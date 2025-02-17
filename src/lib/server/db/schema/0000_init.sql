749e3d234efd59e35d89d1bd9d1e1f5ec9f0ac4c
Rebecca Parks <git@rlparks.net>
2025-02-17 16:34:50 -0500

    Create initial schema file

---
+++ src/lib/server/db/schema/0000_init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS users (
+  id TEXT PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+  id TEXT PRIMARY KEY,
+  hashed_token TEXT NOT NULL,
+  user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  id_token TEXT NOT NULL,
+  ip_address TEXT
+);
+
+DO $$
+BEGIN
+    CREATE TYPE roles AS ENUM ('admin', 'superadmin');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+
+CREATE TABLE IF NOT EXISTS user_roles (
+  id TEXT PRIMARY KEY,
+  user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  role roles NOT NULL
+);
