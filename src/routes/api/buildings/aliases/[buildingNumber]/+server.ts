f42b679efc0023b2afdb65859327381bfa98b735
Rebecca Parks <git@rlparks.net>
2025-03-17 15:44:53 -0400

    Add PUT /api/buildings/aliases/[buildingNumber]

--- src/routes/api/buildings/aliases/[buildingNumber]/+server.ts
+++ src/routes/api/buildings/aliases/[buildingNumber]/+server.ts
@@ -1,4 +1,6 @@
-import { json } from "@sveltejs/kit";
+import { buildingAliasIsValid } from "$lib/types/entity/guards";
+import { validateBuildingAliasFields } from "$lib/types/entity/helpers";
+import { error, json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const GET: RequestHandler = async (event) => {
@@ -8,4 +10,19 @@ export const GET: RequestHandler = async (event) => {
 	const alias =
 		await event.locals.db.buildingAliases.getBuildingAliasByBuildingNumber(buildingNumber);
 	return json(alias);
+};
+
+export const PUT: RequestHandler = async (event) => {
+	event.locals.security.isSuperadmin();
+
+	const buildingNumber = event.params.buildingNumber;
+	const body = await event.request.json();
+	if (buildingAliasIsValid(body) && validateBuildingAliasFields(body)) {
+		const alias = await event.locals.db.buildingAliases.updateBuildingAliasById(
+			buildingNumber,
+			body,
+		);
+		return json(alias);
+	}
+	return error(400, "Invalid building alias");
 };
