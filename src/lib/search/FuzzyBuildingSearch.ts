9fd252af1db27afbd6360a996104502ab757c29f
Rebecca Parks <git@rlparks.net>
2025-03-24 12:05:38 -0400

    Correct types for building search

--- src/lib/search/FuzzyBuildingSearch.ts
+++ src/lib/search/FuzzyBuildingSearch.ts
@@ -1,17 +1,17 @@
-import type { Building, BuildingWithAlias } from "$lib/types";
+import type { BuildingWithAlias } from "$lib/types";
 import Fuse from "fuse.js";
 import { FuzzySearch } from "./FuzzySearch";
 
 /**
  * Fuzzy search for {@link Building}s.
  */
-export class FuzzyBuildingSearch extends FuzzySearch<Building> {
+export class FuzzyBuildingSearch extends FuzzySearch<BuildingWithAlias> {
 	fuse: Fuse<BuildingWithAlias>;
 	private static FUSE_BUILDING_OPTIONS = {
 		keys: ["name", "number", "alias"],
 	};
 
-	constructor(items: Building[]) {
+	constructor(items: BuildingWithAlias[]) {
 		super(items);
 
 		this.fuse = new Fuse(items, FuzzyBuildingSearch.FUSE_BUILDING_OPTIONS);
